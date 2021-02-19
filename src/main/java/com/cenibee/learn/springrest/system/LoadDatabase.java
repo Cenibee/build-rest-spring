e004d0c7fac466af2e80e95073413463096c2ac7
Cenibee <30590761+Cenibee@users.noreply.github.com>
2021-02-19 15:19:36 +0900

    :sparkles: Adding new Domain: Employee and Employee PreLoader

---
+++ src/main/java/com/cenibee/learn/springrest/system/LoadDatabase.java
@@ -0,0 +1,23 @@
+package com.cenibee.learn.springrest.system;
+
+import com.cenibee.learn.springrest.employee.Employee;
+import com.cenibee.learn.springrest.employee.EmployeeRepository;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class LoadDatabase {
+
+    private static final Logger log = LoggerFactory.getLogger(LoadDatabase.class);
+
+    @Bean
+    CommandLineRunner initDatabase(EmployeeRepository repository) {
+        return args -> {
+            log.info("Preloading " + repository.save(new Employee("Bilbo Baggins", "burglar")));
+            log.info("Preloading " + repository.save(new Employee("Frodo Baggins", "thief")));
+        };
+    }
+}
