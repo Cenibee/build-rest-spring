374a43cad31aac24a9f585aa157aaf6db093da32
Cenibee <30590761+Cenibee@users.noreply.github.com>
2021-02-26 16:49:34 +0900

    :label: Applying HATEOS to GET "/employees"

--- src/main/java/com/cenibee/learn/springrest/employee/EmployeeController.java
+++ src/main/java/com/cenibee/learn/springrest/employee/EmployeeController.java
@@ -1,11 +1,20 @@
 package com.cenibee.learn.springrest.employee;
 
-import org.springframework.hateoas.EntityModel;
-import org.springframework.web.bind.annotation.*;
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.linkTo;
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.methodOn;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
-import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.*;
+import org.springframework.hateoas.CollectionModel;
+import org.springframework.hateoas.EntityModel;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class EmployeeController {
@@ -17,8 +26,16 @@ public class EmployeeController {
     }
 
     @GetMapping("/employees")
-    List<Employee> all() {
-        return this.repository.findAll();
+    CollectionModel<EntityModel<Employee>> all() {
+    	
+    	List<EntityModel<Employee>> employees = repository.findAll().stream()
+    			.map(employee -> EntityModel.of(employee,
+    					linkTo(methodOn(EmployeeController.class).one(employee.getId())).withSelfRel(),
+    					linkTo(methodOn(EmployeeController.class).all()).withRel("employees")))
+    			.collect(Collectors.toList());
+    	
+    	return CollectionModel.of(employees,
+    			linkTo(methodOn(EmployeeController.class).all()).withSelfRel());
     }
 
     @PostMapping("/employees")
